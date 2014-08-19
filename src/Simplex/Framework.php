9fe2c5052f1af407432406b3f83d2de49e556bfc
xxvholic <xxvholic@howzhi.com>
2014-08-20 00:24:50 +0800

    fix

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,45 +2,24 @@
  
 namespace Simplex;
  
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel;
  
-class Framework extends HttpKernel implements HttpKernelInterface
+class Framework extends HttpKernel\HttpKernel implements HttpKernel\HttpKernelInterface
 {
-    // protected $matcher;
-    // protected $resolver;
-    // protected $dispatcher;
+    public function __construct($routes)
+    {
+        $context = new Routing\RequestContext();
+        $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+        $resolver = new HttpKernel\Controller\ControllerResolver();
  
-    // public function __construct(EventDispatcher $dispatcher, UrlMatcherInterface $matcher, ControllerResolverInterface $resolver)
-    // {
-    //     $this->matcher = $matcher;
-    //     $this->resolver = $resolver;
-    //     $this->dispatcher = $dispatcher;
-    // }
+        $dispatcher = new EventDispatcher();
+        $dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
+        $dispatcher->addSubscriber(new HttpKernel\EventListener\ExceptionListener('Controller\\ErrorController::exceptionAction'));
+        $dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
+        $dispatcher->addSubscriber(new StringResponseListener());
  
-    // public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
-    // {
-    //     try {
-    //         $request->attributes->add($this->matcher->match($request->getPathInfo()));
- 
-    //         $controller = $this->resolver->getController($request);
-    //         $arguments = $this->resolver->getArguments($request, $controller);
- 
-    //         $response = call_user_func_array($controller, $arguments);
-    //     } catch (ResourceNotFoundException $e) {
-    //         $response = new Response('Not Found', 404);
-    //     // } catch (\Exception $e) {
-    //     //     $response = new Response('An error occurred', 500);
-    //     }
-
-    //     $this->dispatcher->dispatch('response', new ResponseEvent($response, $request));
-
-    //     return $response;
-    // }
+        parent::__construct($dispatcher, $resolver);
+    }
 }
--- web/index.php
+++ web/index.php
@@ -3,29 +3,9 @@
 require_once __DIR__.'/../vendor/autoload.php';
  
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
-$context = new Routing\RequestContext();
-$context->fromRequest($request);
-
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-
-$dispatcher = new EventDispatcher();
-
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ExceptionListener('Controller\\ErrorController::exceptionAction'));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-
-// $dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-
-
-$framework = new Simplex\Framework($dispatcher, $resolver);
+$framework = new Simplex\Framework($routes);
 $framework->handle($request)->send();
