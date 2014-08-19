4d30a98f70a4e9bf3111f9237d8dc942a1820d7f
xxvholic <xxvholic@howzhi.com>
2014-08-19 14:17:02 +0800

    fix

--- web/index.php
+++ web/index.php
@@ -1,25 +1,30 @@
 <?php
  
-require_once __DIR__.'/../src/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
  
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
  
 $request = Request::createFromGlobals();
  
-$map = array(
-    '/hello' => 'hello'
-);
+$routes = include __DIR__.'/../app/app.php';
+
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
  
 $path = $request->getPathInfo();
 
-if (isset($map[$path])) {
-	ob_start();
-	extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
     $response = new Response(ob_get_clean());
-} else {
+} catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
 }
  
 $response->send();
